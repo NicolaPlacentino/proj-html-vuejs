e3aeb714d6bfbda6eef7533a273c95cadff9aff7
NicolaPlacentino <nyko0921@gmail.com>
2023-01-31 11:43:37 +0100

    imported images and created macro components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script>
 import { store } from './store';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 export default {
   name: 'App',
+  components: { AppHeader, AppMain, AppFooter, },
   data() {
     return {
       store,
@@ -11,6 +15,9 @@ export default {
 </script>
 
 <template>
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
 
 </template>
 
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<script>
+import { store } from './store';
+export default {
+    name: 'AppFooter',
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
