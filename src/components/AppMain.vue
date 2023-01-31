50495a4143c41453d9c72bc9d9e7c6b06d707e6c
NicolaPlacentino <nyko0921@gmail.com>
2023-01-31 13:33:56 +0100

    added AppMain structure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,38 @@
 <script>
 import { store } from '../store';
+import OrderingForm from './OrderingForm.vue';
+import BaseCarousel from './BaseCarousel.vue';
 export default {
     name: 'AppMain',
     data() {
         return {
             store,
         }
-    }
+    },
+    components: { BaseCarousel, OrderingForm }
 }
 </script>
 
 <template>
+    <BaseCarousel></BaseCarousel>
+    <div>
 
+    </div>
+    <BaseCarousel></BaseCarousel>
+
+    <section class="specials">
+
+    </section>
+    <div>
+
+    </div>
+    <section class="menu">
+
+    </section>
+    <section class="deals">
+
+    </section>
+    <OrderingForm></OrderingForm>
 </template>
 
 <style lang="scss" scoped>
